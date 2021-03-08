94ef5bb89e08cacbd0c6d927874fff1e64d0096f
Pierre Lin <pslin@ualberta.ca>
2021-03-07 19:11:27 -0800

    Displays map in new tab, centered on user

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import datetime
 import pandas as pd
 import numpy as np
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+
+import folium
+import geocoder
 
 app = Flask(__name__)
+
+def generate_map():
+    myloc = geocoder.ip('me')
+
+    map = folium.Map(location = myloc.latlng, zoom_start = 13)
+
+    return map
 
 @app.route('/')
 def index():
 
+    #Create single column dataframe from csv of TLE
     df = pd.read_csv('active.txt', delimiter = "\n", header= None)
 
+    #Reshape dataframe into three column dataframe
     #Is there a better way to do this? Instead of reading in as a dataframe then reshaping, can we read it in a 3 column data frame?
     #https://stackoverflow.com/questions/39761366/transpose-the-data-in-a-column-every-nth-rows-in-pandas
     #Credit: jezrael (https://stackoverflow.com/users/2901002/jezrael)
     new_df = pd.DataFrame(np.reshape(df.values,(int(df.shape[0] / 3),3)),columns=['Name','Line 1','Line 2'])
 
+    #Parse TLE data
     for index, row in new_df.iterrows():
         tle_rec = ephem.readtle(row['Name'], row['Line 1'], row['Line 2'])
         tle_rec.compute()
@@ -31,8 +44,14 @@ def index():
     # tle_rec.compute()
     #
     # print(tle_rec.sublong, tle_rec.sublat, tle_rec.elevation)
+
     return render_template('index.html')
 
+@app.route('/map')
+def show_map():
+    map = generate_map()
+
+    return map._repr_html_()
 
 # if __name__ == '__main__':
 #     main()
